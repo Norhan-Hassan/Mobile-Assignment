9f2295c854082af1a9e43ac770dd5607f8b467a3
Norhan Hassan <nwrhanhsnly@gmail.com>
2024-04-06 05:05:41 +0200

    add image picker

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 import 'edit_profile_screen.dart';
 
 class User {
@@ -7,8 +11,6 @@ class User {
   final String gender;
   final String studentId;
   final String level;
-  //final String password;
-
 
   const User({
     required this.name,
@@ -16,7 +18,6 @@ class User {
     required this.gender,
     required this.studentId,
     required this.level,
-    //required this.password,
   });
 }
 
@@ -26,13 +27,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'User Profile',
       theme: ThemeData(
-        primarySwatch: Colors.indigo, // Change primary color to indigo
+        primarySwatch: Colors.indigo,
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo), // Set button background color
-            textStyle: MaterialStateProperty.all<TextStyle>(TextStyle(fontSize: 20, color: Colors.white)), // Set button text style
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo),
+            textStyle: MaterialStateProperty.all<TextStyle>(TextStyle(fontSize: 20, color: Colors.white)),
             padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 40, vertical: 12)),
           ),
+        ),
+      ),
+      home: ProfileScreen(
+        user: const User(
+          name: "John Doe",
+          email: "john.doe@example.com",
+          gender: "Male",
+          studentId: "123456",
+          level: "3",
         ),
       ),
     );
@@ -50,11 +60,25 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   late User _currentUser;
+  File? _image;
 
   @override
   void initState() {
     super.initState();
     _currentUser = widget.user;
+  }
+
+  Future<void> _getImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: source);
+
+    setState(() {
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
+      } else {
+        print('No image selected.');
+      }
+    });
   }
 
   void updateUser(User newUser) {
@@ -69,7 +93,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: Text('Your Profile'),
       ),
-      body: ProfileBody(user: _currentUser, onUpdate: updateUser),
+      body: ProfileBody(user: _currentUser, onUpdate: updateUser, image: _image, getImage: _getImage),
     );
   }
 }
@@ -77,8 +101,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
 class ProfileBody extends StatelessWidget {
   final User user;
   final Function(User) onUpdate;
+  final File? image;
+  final Function(ImageSource) getImage;
 
-  ProfileBody({required this.user, required this.onUpdate});
+  ProfileBody({required this.user, required this.onUpdate, required this.image, required this.getImage});
 
   @override
   Widget build(BuildContext context) {
@@ -87,14 +113,19 @@ class ProfileBody extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          if (image != null)
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: FileImage(image!),
+            ),
           Text(
             'Name: ${user.name}',
-            style: TextStyle(fontSize: 20, color: Colors.black), // Larger font size, indigo color, bold
+            style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
           Text(
             'Email: ${user.email}',
-            style: TextStyle(fontSize: 18, color: Colors.black), // Smaller font size, indigo color
+            style: TextStyle(fontSize: 18, color: Colors.black),
           ),
           SizedBox(height: 8),
           Text(
@@ -114,6 +145,44 @@ class ProfileBody extends StatelessWidget {
           SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Select Image Source'),
+                    content: SingleChildScrollView(
+                      child: ListBody(
+                        children: [
+                          GestureDetector(
+                            child: Text('Gallery'),
+                            onTap: () {
+                              getImage(ImageSource.gallery);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          SizedBox(height: 20),
+                          GestureDetector(
+                            child: Text('Camera'),
+                            onTap: () {
+                              getImage(ImageSource.camera);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            child: Text(
+              'Change Profile Photo',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => EditProfileScreen(user: user, onUpdate: onUpdate)),
@@ -121,7 +190,7 @@ class ProfileBody extends StatelessWidget {
             },
             child: Text(
               'Edit Profile',
-              style: TextStyle(fontSize: 20), // Larger font size for button text
+              style: TextStyle(fontSize: 20),
             ),
           ),
         ],
